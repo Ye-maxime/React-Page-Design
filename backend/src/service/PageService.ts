dd3dc2a897164ea9455176dbfd7abb9765b8199e
maxime <yzmmaxime@163.com>
2020-10-25 17:17:00 +0100

    improve service

--- backend/src/service/PageService.ts
+++ backend/src/service/PageService.ts
@@ -15,22 +15,23 @@ export class PageService {
   }
 
   async addPage(page: any) {
-    const newPage = new Page();
-    newPage.pageId = page.pageId;
-    newPage.name = page.name;
-    newPage.elements = [];
+    // const newPage = new Page();
+    // newPage.pageId = page.pageId;
+    // newPage.name = page.name;
+    // newPage.elements = [];
 
     const pageRepository = getMongoRepository(Page);
-    const res = await pageRepository.save(newPage);
+    const res = await pageRepository.save(page);
     return res;
   }
 
   async updatePage(id: string, page: any) {
     const pageRepository = getMongoRepository(Page);
-    const res = await pageRepository.update(
-      { pageId: id },
-      { name: page.name, elements: page.elements }
-    );
+    const res = await pageRepository.update({ pageId: id }, page);
+    // const res = await pageRepository.update(
+    //   { pageId: id },
+    //   { name: page.name, elements: page.elements }
+    // );
     return res;
   }
 }
